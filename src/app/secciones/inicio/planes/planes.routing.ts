d4bd94e589ad805417e442edf550d0109cdcc364
RogerBariles <rogerbariles@gmail.com>
2021-11-03 15:46:56 -0300

    correccion seccion planes

--- src/app/secciones/inicio/planes/planes.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import { PlanesComponent } from "./planes.component";
-
-export const routesPlanes: Routes = [
-    {
-        path: '',
-        component: PlanesComponent
-    }
-];
